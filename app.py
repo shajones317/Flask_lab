24899da7e7dacc061f4fdaa10f013c6ad127b7a2
Shawn Jones <jones_shawn2@students.columbusstate.edu>
2024-02-04 19:05:30 +0000

    Flask_lab

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template                                   # import flask framework
+from flask import Flask, render_template, request
 
-app = Flask(__name__)                                                      # create an app instance
+app = Flask(__name__)
 
-@app.route("/")                                                            # use the home url
-def hello():                                                               # method called hello
-    return render_template("index.html")                                                 # returns "hello world"
+@app.route("/")
+def hello():
+    return render_template("index.html")
 
-if __name__ == "__main__":                                                 # when running python app.py
-    app.run(debug=True)                                                              # run the flask appS
+@app.route("/about")
+def about():
+    # Check if 'name' is in the query parameters, if not, use a default value
+    name = request.args.get('name') if request.args.get('name') else "Hello World!"
+    return render_template("about.html", aboutName=name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
